b7899a4de56f487c7aef09891b62e34ee9d13dfe
chn2189724319-creator <chn2189724319@gmail.com>
2026-03-27 17:21:37 +0800

    Update 启动器-v终末之诗-meta.js

--- 启动器-v终末之诗-meta.js
+++ 启动器-v终末之诗-meta.js
@@ -8,6 +8,6 @@
 // @include      file:///*
 // @run-at       document-start
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/chn2189724319-creator/--4.5.0.js/main/%E7%BC%96%E8%BE%91%E6%A8%A1%E5%BC%8F%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-%E5%8F%AF%E7%BD%91%E7%BB%9C%E6%9B%B4%E6%96%B0.meta.js
-// @downloadURL  https://raw.githubusercontent.com/chn2189724319-creator/--4.5.0.js/main/%E7%BC%96%E8%BE%91%E6%A8%A1%E5%BC%8F%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-%E5%8F%AF%E7%BD%91%E7%BB%9C%E6%9B%B4%E6%96%B0.user.js
+// @updateURL    https://raw.githubusercontent.com/chn2189724319-creator/jiaotiao/refs/heads/main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-meta.js
+// @downloadURL  https://raw.githubusercontent.com/chn2189724319-creator/jiaotiao/refs/heads/main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-user.js
 // ==/UserScript==
