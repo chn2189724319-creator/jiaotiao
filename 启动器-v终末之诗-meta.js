aa816fe1b5a44e7c22c21dc1af342d84a58b3f7c
chn2189724319-creator <chn2189724319@gmail.com>
2026-03-27 17:19:25 +0800

    Add files via upload

---
+++ 启动器-v终末之诗-meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         编辑模式启动器 v终末之诗（IOS 版）
+// @namespace    http://tampermonkey.local/
+// @version      1.0.0
+// @description  联网自动更新启动器（WebCrypto AES-GCM + Brotli 解密，兼容新加密格式）
+// @author       陈浩南
+// @match        *://xgxt.huhst.edu.cn/*
+// @include      file:///*
+// @run-at       document-start
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/chn2189724319-creator/--4.5.0.js/main/%E7%BC%96%E8%BE%91%E6%A8%A1%E5%BC%8F%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-%E5%8F%AF%E7%BD%91%E7%BB%9C%E6%9B%B4%E6%96%B0.meta.js
+// @downloadURL  https://raw.githubusercontent.com/chn2189724319-creator/--4.5.0.js/main/%E7%BC%96%E8%BE%91%E6%A8%A1%E5%BC%8F%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-%E5%8F%AF%E7%BD%91%E7%BB%9C%E6%9B%B4%E6%96%B0.user.js
+// ==/UserScript==
