662c42b61045490bd917f770f4378c3f8b28939a
chn2189724319-creator <chn2189724319@gmail.com>
2026-03-27 20:37:49 +0800

    Update 启动器-v终末之诗-meta.js

--- 启动器-v终末之诗-meta.js
+++ 启动器-v终末之诗-meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         启动器（新）v终末之诗
 // @namespace    http://tampermonkey.local/
-// @version      1.0.2
+// @version      1.0.3
 // @description  联网自动更新启动器
 // @author       匿名
 // @match        *://xgxt.huhst.edu.cn/*
 // @include      file:///*
 // @run-at       document-start
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/chn2189724319-creator/jiaotiao/refs/heads/main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-meta.js
-// @downloadURL  https://raw.githubusercontent.com/chn2189724319-creator/jiaotiao/refs/heads/main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/chn2189724319-creator/jiaotiao@main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-meta.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/chn2189724319-creator/jiaotiao@main/%E5%90%AF%E5%8A%A8%E5%99%A8-v%E7%BB%88%E6%9C%AB%E4%B9%8B%E8%AF%97-user.js
 // ==/UserScript==
